d5f95fcb6410bcdbb460855cd7f264973d7e9b94
shiv.prakash <shiv.prakash@cgi.com>
2019-08-01 09:41:58 +0530

    user service

---
+++ src/main/java/com/stackroute/userservice/UserServiceApplication.java
@@ -0,0 +1,12 @@
+package com.stackroute.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class UserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceApplication.class, args);
+	}
+
+}
